9ddbc10548a21cc24e9df960f5d71ffe549a0b7e
tahirarain <tahirarain085@gmail.com>
2021-10-07 22:34:32 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,15 @@
+const colorBtn = document.querySelector('.btn-color');
+const bodybcg = document.querySelector('body');
+const colors = ['red','green','blue','pink','#ef777f'];
+
+
+colorBtn.addEventListener('click',changeColor);
+
+function changeColor (){
+    // bodybcg.style.backgroundColor = colors[4];
+    /* make a variable by using math.floor and math random method  */
+
+    let random = Math.floor(Math.random()*colors.length);
+    bodybcg.style.backgroundColor = colors[random]
+
+}
